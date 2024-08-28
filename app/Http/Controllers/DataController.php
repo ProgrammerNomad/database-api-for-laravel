c4f22a3f8515370d185dc6121d92151966a077f4
Shiv Singh <shiv@srapsware.com>
2024-08-28 16:50:11 +0530

    Refactor DataController to update or insert data based on domain

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Data;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Http;
 
 class DataController extends Controller
@@ -41,13 +42,33 @@ class DataController extends Controller
 
             // Now you can work with the $xdata array/object
             // For example, access a specific field:
-            //$someValue = $xdata['some_field'];
+            $offset = $xdata['NextOffset'];
 
             // Or iterate over an array of results:
             foreach ($xdata['Results'] as $result) {
 
-                print_r($result);
+                $newData = array();
 
+                $domain = $result['D'];
+
+                $newData['domain'] = $result['D'];
+                $newData['Social'] = $result['D'];
+                $newData['CompanyName'] = $result['D'];
+                $newData['Telephones'] = $result['D'];
+                $newData['Emails'] = $result['D'];
+                $newData['Titles'] = $result['D'];
+                $newData['State'] = $result['D'];
+                $newData['Postcode'] = $result['D'];
+                $newData['Country'] = $result['D'];
+                $newData['Vertical'] = $result['D'];
+
+
+
+                // if domain exists then update else insert new row
+                echo '<pre>';
+                print_r($result);
+                echo '</pre>';
+                //$this->updateOrInsertData($domain, $result);
 
             }
         } else {
@@ -55,17 +76,56 @@ class DataController extends Controller
             // Log the error, return an error view, etc.
         }
 
-
-        // check for existing data in the database
-
-
-        // if data exists, update the data for only email and phone number
-
-
         // Save offset to database so that we can continue from where we stopped
 
 
         return response()->json($results);
+    }
+
+    public function updateOrInsertData($domain, $newData)
+    {
+        if (Schema::hasColumn('data', 'domain')) {
+            $existingData = DB::table('data')
+                ->where('domain', $domain)
+                ->first();
+
+            if ($existingData) {
+                // Decode JSON data from each column
+                $socialData = json_decode($existingData->Social, true) ?? [];
+                $telephonesData = json_decode($existingData->Telephones, true) ?? [];
+                $emailsData = json_decode($existingData->Emails, true) ?? [];
+                $titlesData = json_decode($existingData->Titles, true) ?? [];
+
+                // Merge new data with existing data for each column
+                $mergedSocial = array_merge($socialData, $newData['Social'] ?? []);
+                $mergedTelephones = array_merge($telephonesData, $newData['Telephones'] ?? []);
+                $mergedEmails = array_merge($emailsData, $newData['Emails'] ?? []);
+                $mergedTitles = array_merge($titlesData, $newData['Titles'] ?? []);
+
+                DB::table('data')
+                    ->where('domain', $domain)
+                    ->update([
+                        'Social' => json_encode($mergedSocial),
+                        'Telephones' => json_encode($mergedTelephones),
+                        'Emails' => json_encode($mergedEmails),
+                        'Titles' => json_encode($mergedTitles)
+                    ]);
+            } else {
+                // Insert new record if it doesn't exist
+                DB::table('data')->insert([
+                    'domain' => $domain,
+                    'Social' => json_encode($newData['Social'] ?? []),
+                    'Telephones' => json_encode($newData['Telephones'] ?? []),
+                    'Emails' => json_encode($newData['Emails'] ?? []),
+                    'Titles' => json_encode($newData['Titles'] ?? []),
+                ]);
+            }
+        } else {
+            // Handle case where the 'domain' column doesn't exist
+            // You might want to log an error or create the column
+            // For example:
+            \Log::error("Column 'domain' not found in table 'data'");
+        }
     }
 
     public function store(Request $request)
