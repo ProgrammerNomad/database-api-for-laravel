7a8f1cc134c8d85b47ebbd28257a0567199f4da7
Shiv Singh <shiv@srapsware.com>
2024-09-01 09:52:10 +0530

    Refactor DataController to include CRUD routes for 'data2' resource

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,15 @@ Route::delete('data/{id}', [DataController::class, 'destroy']);
 
 Route::resource('data', DataController::class);
 
+
+
+Route::get('data2', [DataController::class, 'index']);
+Route::post('data2', [DataController::class, 'store']);
+Route::get('data2/{id}', [DataController::class, 'show']);
+Route::put('data2/{id}', [DataController::class, 'update']);
+Route::delete('data2/{id}', [DataController::class, 'destroy']);
+
+Route::resource('data2', DataController::class);
+
 Route::resource('technologies', TechnologyController::class);
 
