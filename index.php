43eb57d4504c651d7bb7c1367b47b23893847ad6
Shiv Singh <shiv@srapsware.com>
2024-09-01 10:06:38 +0530

    Refactor DataController to update 'require' paths for autoloading and bootstrap files

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = '/var/www/vhosts/mobrilz.digital/data.mobrilz.digital/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = '/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require '/var/www/vhosts/mobrilz.digital/data.mobrilz.digital/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once '/var/www/vhosts/mobrilz.digital/data.mobrilz.digital/bootstrap/app.php')
+(require_once __DIR__.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
